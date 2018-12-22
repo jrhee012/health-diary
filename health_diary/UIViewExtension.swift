c2aa1d031416695a2e6a589dba9589e04fafdd1a
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-22 15:45:11 -0500

    update scroll views

---
+++ health_diary/UIViewExtension.swift
@@ -0,0 +1,44 @@
+//
+//  UIViewExtension.swift
+//  health_diary
+//
+//  Created by Jaehyuk Rhee on 12/22/18.
+//  Copyright © 2018 Jaehyuk Rhee. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.topAnchor
+        } else {
+            return self.topAnchor
+        }
+    }
+    
+    var safeLeftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.leftAnchor
+        }else {
+            return self.leftAnchor
+        }
+    }
+    
+    var safeRightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *){
+            return self.safeAreaLayoutGuide.rightAnchor
+        }else {
+            return self.rightAnchor
+        }
+    }
+    
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.bottomAnchor
+        } else {
+            return self.bottomAnchor
+        }
+    }
+}
