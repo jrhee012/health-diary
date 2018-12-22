ca706017aa6bb83522ed96c3ff628de8988ae37b
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-12-22 13:17:54 -0500

    Initial Commit

---
+++ health_diary/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  health_diary
+//
+//  Created by Jaehyuk Rhee on 12/22/18.
+//  Copyright © 2018 Jaehyuk Rhee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ health_diaryTests/health_diaryTests.swift
@@ -0,0 +1,34 @@
+//
+//  health_diaryTests.swift
+//  health_diaryTests
+//
+//  Created by Jaehyuk Rhee on 12/22/18.
+//  Copyright © 2018 Jaehyuk Rhee. All rights reserved.
+//
+
+import XCTest
+@testable import health_diary
+
+class health_diaryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
